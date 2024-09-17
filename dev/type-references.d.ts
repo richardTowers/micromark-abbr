18b4ef69332696d8563cdc4e2d72efbe97b99d1e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-09-17 12:57:21 +0100

    Use imports instead of references
    To appease the linter

--- dev/type-references.d.ts
+++ dev/type-references.d.ts
@@ -1,2 +1,2 @@
-/// <reference types="./lib/mdast-util-abbr/types/index.js" />
-/// <reference types="./lib/micromark-extension-abbr/types/index.js" />
+import type * as _mdast_util from './lib/mdast-util-abbr/types/index.js'
+import type * as _micromark_extension from './lib/micromark-extension-abbr/types/index.js'
